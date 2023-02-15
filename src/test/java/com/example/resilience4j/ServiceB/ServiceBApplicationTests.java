fae65798338c705e843c5f6a993363cc56a45de7
Eeranna Kuruva <veeruamikable@gmail.com>
2023-02-15 16:46:29 +0530

    first commit

---
+++ src/test/java/com/example/resilience4j/ServiceB/ServiceBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resilience4j.ServiceB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
